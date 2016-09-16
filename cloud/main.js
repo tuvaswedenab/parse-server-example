cedf7a98b4946aac1fbe48cba97c6d937fb362c9
Christoffer Assgård <a.christoffer@outlook.com>
2016-09-16 20:36:28 +0200

    Update function semi done

--- cloud/main.js
+++ cloud/main.js
@@ -10,6 +10,83 @@ Parse.Cloud.define("checkAPP", function(request, response){
 	response.success(Parse.applicationId)
 });
 
+Parse.Cloud.define("UpdateInstallation", function(request, response) {
+	// Set up to modify user data
+    Parse.Cloud.useMasterKey();
+        
+    if (!request.params.objectId) { 
+        createNewInstallation(request, { 
+            success: function(installation) {
+                response.success('Successfully updated installation table.');
+            }, error: function(installation, error) {
+                response.error(error);
+            }
+        });
+    } else {
+        var query = new Parse.Query(Parse.Installation);
+        query.equalTo("objectId", request.params.objectId);
+        query.first({
+            success: function(installation) {
+                if  (installation) {
+                    // The object was found, update it.
+                    installation.set("GCMSenderId", request.params.GCMSenderId);
+                    installation.set("installationId", request.params.installationId);
+                    installation.set("deviceType", request.params.deviceType);
+                    installation.set("appName", request.params.appName);
+                    installation.set("appIdentifier", request.params.appIdentifier);
+                    installation.set("parseVersion", request.params.parseVersion);
+                    installation.set("deviceToken", request.params.deviceToken);
+                    installation.set("pushType", request.params.pushType);
+                    installation.set("timeZone", request.params.timeZone);
+                    installation.set("localeIdentifier", request.params.localeIdentifier);
+                    installation.set("appVersion", request.params.appVersion);
+
+                    installation.save(null, {
+                        success: function(installation) {
+                            response.success('Successfully updated installation table.');
+                        }, error: function(installation, error) {
+                            response.error("Could not save changes to installation.");
+                        }
+                    });
+                } else {
+                    createNewInstallation(request, { 
+                        success: function(installation) {
+                            response.success('Successfully updated installation table.');
+                        }, error: function(installation, error) {
+                            response.error("Could not save changes to installation.");
+                        }
+                    });
+                }
+            }, error: function(error) {
+                response.error('query error');
+            }
+        });
+    }
+});
+
+var createNewInstallation = function(request, response) {
+    Parse.Cloud.useMasterKey();
+    var installation = new Parse.Installation();
+    installation.set("GCMSenderId", request.params.GCMSenderId);
+    installation.set("installationId", request.params.installationId);
+    installation.set("deviceType", request.params.deviceType);
+    installation.set("appName", request.params.appName);
+    installation.set("appIdentifier", request.params.appIdentifier);
+    installation.set("parseVersion", request.params.parseVersion);
+    installation.set("deviceToken", request.params.deviceToken);
+    installation.set("pushType", request.params.pushType);
+    installation.set("timeZone", request.params.timeZone);
+    installation.set("localeIdentifier", request.params.localeIdentifier);
+    installation.set("appVersion", request.params.appVersion);
+    installation.save(null, {
+        success: function(installation) {
+            response.success(installation);
+        }, error: function(installation, error) {
+            response.error(installation, error);
+        }
+    });
+}
+
 Parse.Cloud.define("changeUserPassword", function(request, response) {
 	 	// Set up to modify user data
 	 	Parse.Cloud.useMasterKey();
