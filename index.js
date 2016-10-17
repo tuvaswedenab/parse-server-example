24682b4000f432b402e68b6c0cba634559c0bad7
Carlos Martin (SE) <carlos.martin@tuva.co>
2016-10-17 16:11:06 +0200

    Adding mailgun settings

--- index.js
+++ index.js
@@ -5,6 +5,41 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
+//MAILGUN ADAPTER
+var Mailgun = require('mailgun-js');
+
+var SimpleMailgunAdapter = mailgunOptions => {
+  if (!mailgunOptions || !mailgunOptions.apiKey || !mailgunOptions.domain || !mailgunOptions.fromAddress) {
+    throw 'SimpleMailgunAdapter requires an API Key, domain, and fromAddress.';
+  }
+  var mailgun = Mailgun(mailgunOptions);
+
+  var sendMail = mail => {
+    var data = {
+      from: mailgunOptions.fromAddress,
+      to: mail.to,
+      subject: mail.subject,
+      text: mail.text,
+    }
+
+    return new Promise((resolve, reject) => {
+      mailgun.messages().send(data, (err, body) => {
+        if (typeof err !== 'undefined') {
+          reject(err);
+        }
+        resolve(body);
+      });
+    });
+  }
+
+  return Object.freeze({
+    sendMail: sendMail
+  });
+}
+
+module.exports = SimpleMailgunAdapter
+//END MAILGUN ADAPTER
+
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
